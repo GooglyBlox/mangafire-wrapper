ee8f13478db07f554e9fac58b3ac8ae17bc5a45b
GooglyBlox <cemcimyildiz@gmail.com>
2023-08-17 13:50:05 -0700

    Try to get around Google Auth

--- main.js
+++ main.js
@@ -12,6 +12,8 @@ function createWindow() {
     }
   });
 
+  win.webContents.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537');
+
   win.loadURL('https://mangafire.to');
 }
 
