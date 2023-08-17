cb9648189693bf77ddd6f7b399a20c5d7708497b
GooglyBlox <cemcimyildiz@gmail.com>
2023-08-17 13:23:27 -0700

    Remove a few unnecessary comments

--- main.js
+++ main.js
@@ -1,25 +1,22 @@
 const { app, BrowserWindow } = require('electron');
-const path = require('path'); // Require the path module
+const path = require('path');
 
 function createWindow() {
-  // Create the browser window.
   const win = new BrowserWindow({
     width: 800,
     height: 600,
-    icon: path.join(__dirname, 'assets/icon.ico'), // Path to your icon file
+    icon: path.join(__dirname, 'assets/icon.ico'),
     webPreferences: {
-      nodeIntegration: false, // Disabling nodeIntegration for security
-      contextIsolation: true  // Protecting against possible execution of remote code
+      nodeIntegration: false,
+      contextIsolation: true
     }
   });
 
-  // Load the website
   win.loadURL('https://mangafire.to');
 }
 
 app.whenReady().then(createWindow);
 
-// Quit when all windows are closed.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
